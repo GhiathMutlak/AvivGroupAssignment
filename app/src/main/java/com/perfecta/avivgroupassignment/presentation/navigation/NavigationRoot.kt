7f36b52b7d3628cf955ec8b915c629446083f86b
Giyas MITLAK <giyas@mantasolutions.online>
2026-01-20 10:17:46 +0100

    Implement Snackbar for error handling in NavigationRoot
    - Integrate `Scaffold` and `SnackbarHost` to provide a container for UI feedback
    - Implement event collection in `NavigationRoot` to display error messages via `SnackbarHostState`

--- app/src/main/java/com/perfecta/avivgroupassignment/presentation/navigation/NavigationRoot.kt
+++ app/src/main/java/com/perfecta/avivgroupassignment/presentation/navigation/NavigationRoot.kt
@@ -1,8 +1,12 @@
 package com.perfecta.avivgroupassignment.presentation.navigation
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.perfecta.avivgroupassignment.presentation.adaptiveListingScreen.AdaptiveListingsScreen
@@ -14,19 +18,24 @@ import kotlinx.coroutines.flow.collectLatest
 fun NavigationRoot() {
     val viewModel: AdaptiveListingsViewModel = hiltViewModel()
     val state by viewModel.state.collectAsStateWithLifecycle()
+    val snackbarHostState = remember { SnackbarHostState() }
 
     LaunchedEffect(Unit) {
         viewModel.events.collectLatest { event ->
             when (event) {
                 is AdaptiveScreenContract.Event.ShowError -> {
-                    // TODO: Show error
+                    snackbarHostState.showSnackbar(event.message)
                 }
             }
         }
     }
 
-    AdaptiveListingsScreen(
-        state = state,
-        onAction = viewModel::onAction
-    )
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) }
+    ) { _ ->
+        AdaptiveListingsScreen(
+            state = state,
+            onAction = viewModel::onAction
+        )
+    }
 }
